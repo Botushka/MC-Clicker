b731d6f4f5e41dcb2bc69a788510bcb96f883cd2
BuildTools <91798274+Botushka@users.noreply.github.com>
2023-01-01 21:51:58 +0200

    3.0

--- src/main/java/me/jaromir/mcclicker/Listener/CookieListener.java
+++ src/main/java/me/jaromir/mcclicker/Listener/CookieListener.java
@@ -1,5 +1,6 @@
 package me.jaromir.mcclicker.Listener;
 
+import me.jaromir.mcclicker.GUI.GUICommand;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -7,11 +8,31 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.Inventory;
 
+public class CookieListener extends GUICommand implements Listener {
 
-public class CookieListener implements Listener {
+    private  int cookies = 0;
+
+    private int upgrade = 0;
+
+    private  int count = 1;
+    public void addCookies(int amount) {
+        count += amount;
+    }
+    public void removeCookies(int amount) {
+        count -= amount;
+    }
+    public void upgradeCost(int cost) {
+        count -= cost;
+    }
+    public void increaseCost(int amount) {
+        upgrade += amount * 2;
+    }
 
     @EventHandler
     public void onRightclick(PlayerInteractEvent e){
@@ -20,7 +41,8 @@ public class CookieListener implements Listener {
         Block block = e.getClickedBlock();
         if(action.equals(Action.RIGHT_CLICK_BLOCK)){
             if(block.getType().equals(Material.HONEY_BLOCK)){
-
+                player.sendMessage("right click");
+                openNewGUI(player);
             }
         }
     }
@@ -28,24 +50,52 @@ public class CookieListener implements Listener {
     @EventHandler
     public void clickEvent(InventoryClickEvent e){
         Player player = (Player) e.getWhoClicked();
-        if(e.getView().getTitle().equals(ChatColor.GOLD + "Cookie Clicker GUI")){
+        if(e.getView().getTitle().equals(ChatColor.GOLD + "Cookie Clicker - Upgardes")){
 
             switch(e.getCurrentItem().getType()){
-                case SHEARS:
-                    player.closeInventory();
-                    player.sendMessage("Clicking power increased by 2");
-                    break;
-                case TOTEM_OF_UNDYING:
-                    player.closeInventory();
-                    player.sendMessage("Clicking power increased by 15");
-                    break;
-                case PISTON:
-                    player.closeInventory();
-                    player.sendMessage("Clicking power increased by 50");
-                    break;
+                if{cookies < }
+                {
+                    case SHEARS:
+                        player.sendMessage("Clicking power increased by 2");
+                        addCookies(2);
+                        upgradeCost(25);
+
+                        break;
+                    case TOTEM_OF_UNDYING:
+                        player.sendMessage("Clicking power increased by 15");
+                        addCookies(15);
+                        upgradeCost(100);
+                        break;
+                    case PISTON:
+                        player.sendMessage("Clicking power increased by 50");
+                        addCookies(400);
+                        break;
+                }
+                else{
+                    player.sendMessage("You can't afford this");
+                }
             }
             e.setCancelled(true);
         }else;
 
     }
+    /**
+     * -Cookie counter when hit
+     */
+
+    @EventHandler
+    public void onBreak(BlockBreakEvent e) {
+        Block block = e.getBlock();
+        Player p = e.getPlayer();
+        String uuid = p.getUniqueId().toString();
+        Material material = block.getType();
+        if (material.equals(Material.HONEY_BLOCK)) {
+            e.setCancelled(true);
+            cookies += count;
+            cookies -= upgrade;
+            p.sendMessage("You now have " + cookies + " cookies!");
+        }
+    }
+
 }
+
--- src/main/java/me/jaromir/mcclicker/MCCLICKER.java
+++ src/main/java/me/jaromir/mcclicker/MCCLICKER.java
@@ -7,25 +7,18 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MCCLICKER extends JavaPlugin implements Listener {
 
-    /***
-     * UNFININSHED
-     * TODO:
-     * -Make GUI commands work how they are supposed to
-     * -Make the GUI more neat and add bit features
-     * -Make right clicking honey block open the gui
-     */
-
     @Override
     public void onEnable() {
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(this, this);
-        getCommand("gui").setExecutor(new GUICommand());
         getServer().getPluginManager().registerEvents(new CookieListener(), this);
     }
 
@@ -38,32 +31,4 @@ public final class MCCLICKER extends JavaPlugin implements Listener {
      * -Default numbers for newcomers
      */
 
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent e) {
-        Player p = e.getPlayer();
-        if (!getConfig().contains("Players." + p.getUniqueId().toString())) {
-            getConfig().set("Players." + p.getUniqueId().toString() + ".cookies", 0);
-            saveConfig();
-        }
-    }
-
-    /**
-     * -Cookie counter when hit
-     */
-
-    @EventHandler
-    public void onBreak(BlockBreakEvent e) {
-        Block block = e.getBlock();
-        Player p = e.getPlayer();
-        String uuid = p.getUniqueId().toString();
-        Material material = block.getType();
-        if (material.equals(Material.HONEY_BLOCK)) {
-            e.setCancelled(true);
-            int cookies = getConfig().getInt("Players." + uuid + ".cookies");
-            getConfig().set("Players." + uuid + ".cookies", cookies +1);
-            saveConfig();
-            p.sendMessage("You now have " + cookies + " cookies!");
-        }
-    }
 }
-
